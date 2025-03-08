661977542430127096e2b3840ea749eec9a8bf85
Tran Quang Loc <quangloctran99@gmail.com>
2025-03-08 21:19:01 +0700

    Make create_prng a function

--- rs/src/prng.rs
+++ rs/src/prng.rs
@@ -1,23 +1,20 @@
 #![allow(dead_code)]
-fn create_prng(seed: u64) -> impl Iterator<Item = u64> {
+fn create_prng(seed: u64) -> impl FnMut() -> u64 {
     use std::collections::hash_map::DefaultHasher;
     use std::hash::Hasher;
     let mut hasher = DefaultHasher::new();
     hasher.write_u64(seed);
 
-    return std::iter::repeat_with(move || {
+    move || {
         let x = hasher.finish();
         hasher.write_u64(x);
         return x;
-    });
+    }
 }
 
 fn seed_from_time_ms() -> u64 {
     use std::time::{SystemTime, UNIX_EPOCH};
-    return SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_millis() as u64;
+    return SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as u64;
 }
 
 #[cfg(test)]
@@ -26,27 +23,28 @@ mod test {
 
     #[test]
     fn test_same_seed() {
-        let prng1 = create_prng(42);
-        let prng2 = create_prng(42);
+        let mut prng1 = create_prng(42);
+        let mut prng2 = create_prng(42);
 
-        for (a, b) in prng1.zip(prng2).take(100) {
-            assert_eq!(a, b);
+        for _ in 0..100 {
+            assert_eq!(prng1(), prng2());
         }
     }
 
     #[test]
     fn test_different_seed() {
-        let prng1 = create_prng(42);
-        let prng2 = create_prng(43);
+        let mut prng1 = create_prng(42);
+        let mut prng2 = create_prng(43);
 
-        for (a, b) in prng1.zip(prng2).take(100) {
-            assert_ne!(a, b);
+        for _ in 0..100 {
+            assert_ne!(prng1(), prng2());
         }
     }
 
     #[test]
     fn test_random_enough_value() {
-        let set: std::collections::HashSet<u64> = create_prng(42).take(10000).collect();
+        let set: std::collections::HashSet<u64> =
+            std::iter::repeat_with(create_prng(42)).take(10000).collect();
         assert_eq!(set.len(), 10000);
     }
 
