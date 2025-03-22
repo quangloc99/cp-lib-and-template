e09500f4428b911e96cf50677b3431678873ceb3
Tran Quang Loc <quangloctran99@gmail.com>
2025-03-22 22:48:32 +0700

    New ModInt template with arbitrary mod

--- rs/src/modint.rs
+++ rs/src/modint.rs
@@ -1,16 +1,27 @@
 #[rustfmt::skip]
 mod modint {
     use std::ops::*;
+    use crate::def_op;
 
-    pub const MOD: usize = 998_244_353;
+    pub trait Modulus: Clone + Copy + PartialEq + Eq + Default {
+        fn m() -> usize;
+    }
 
-    #[derive(Debug, Clone, Copy, Eq, PartialEq)]
-    pub struct ModInt(pub usize);
+    #[derive(Clone, Copy, PartialEq, Eq, Default)]
+    pub struct ConstModulus<const M: usize>;
+    impl<const M: usize> Modulus for ConstModulus<M> {
+        fn m() -> usize { M }
+    }
+
+    #[derive(Clone, Copy, Eq, PartialEq)]
+    pub struct ModInt<Mod: Modulus>(pub usize, pub Mod);
 
     #[allow(dead_code)]
-    impl ModInt {
+    impl<Mod: Modulus> ModInt<Mod> {
+        pub fn m() -> usize { Mod::m() }
+        pub fn raw(x: usize) -> Self { Self(x, Mod::default()) }
         pub fn pow(mut self, mut e: usize) -> Self {
-            let mut result = ModInt(1);
+            let mut result = Self::raw(1);
             while e > 0 {
                 if e & 1 == 1 { result *= self; }
                 self *= self ;
@@ -18,94 +29,87 @@ mod modint {
             }
             result
         }
-        pub fn inv(self) -> Self { self.pow(MOD - 2) }
+        pub fn inv(self) -> Self { self.pow(Mod::m() - 2) }
     }
 
-    macro_rules! def_op {
-        (
-            $type:ident : $trait:ident :: $fn:ident | $a_trait:ident :: $a_fn:ident
-            => (&mut $self_param: ident, $rhs_param: ident) $impl:block
-        ) => {
-            impl<Rhs: Into<$type>> std::ops::$trait<Rhs> for $type {
-                type Output = Self;
-                fn $fn(mut self, rhs: Rhs) -> Self { self.$a_fn(rhs); self }
-            }
-            impl<Rhs: Into<$type>> std::ops::$a_trait<Rhs> for $type {
-                fn $a_fn(&mut $self_param, $rhs_param: Rhs) $impl
-            }
-        };
-    }
+    def_op!(ModInt::<Mod: Modulus>: Add::add | AddAssign::add_assign => (&mut self, rhs) { self.0 += rhs.into().0; if self.0 >= Mod::m() { self.0 -= Mod::m(); } });
+    def_op!(ModInt::<Mod: Modulus>: Sub::sub | SubAssign::sub_assign => (&mut self, rhs) { self.0 = self.0.wrapping_sub(rhs.into().0); if self.0 >= Mod::m() { self.0 = self.0.wrapping_add(Mod::m()); } });
+    def_op!(ModInt::<Mod: Modulus>: Mul::mul | MulAssign::mul_assign => (&mut self, rhs) { self.0 = self.0 * rhs.into().0 % Mod::m(); });
+    def_op!(ModInt::<Mod: Modulus>: Div::div | DivAssign::div_assign => (&mut self, rhs) { *self *= rhs.into().inv(); });
 
-    def_op!(ModInt: Add::add | AddAssign::add_assign => (&mut self, rhs) { self.0 += rhs.into().0; if self.0 >= MOD { self.0 -= MOD; } });
-    def_op!(ModInt: Sub::sub | SubAssign::sub_assign => (&mut self, rhs) { self.0 = self.0.wrapping_sub(rhs.into().0); if self.0 >= MOD { self.0 = self.0.wrapping_add(MOD); } });
-    def_op!(ModInt: Mul::mul | MulAssign::mul_assign => (&mut self, rhs) { self.0 = self.0 * rhs.into().0 % MOD; });
-    def_op!(ModInt: Div::div | DivAssign::div_assign => (&mut self, rhs) { *self *= rhs.into().inv(); });
-
-    impl From<String> for ModInt { fn from(s: String) -> Self { s.parse::<usize>().unwrap().into() } }
-    impl From<u32> for ModInt { fn from(x: u32) -> Self { (x as usize).into() } }
-    impl From<u64> for ModInt { fn from(x: u64) -> Self { (x as usize).into() } }
-    impl From<i32> for ModInt { fn from(x: i32) -> Self { (x as isize).into() } }
-    impl From<i64> for ModInt { fn from(x: i64) -> Self { (x as isize).into() } }
-    impl From<usize> for ModInt { fn from(x: usize) -> Self { ModInt(x % MOD) } }
-    impl From<isize> for ModInt {
+    impl<Mod: Modulus> From<String> for ModInt<Mod> { fn from(s: String) -> Self { s.parse::<usize>().unwrap().into() } }
+    impl<Mod: Modulus> From<u32> for ModInt<Mod> { fn from(x: u32) -> Self { (x as usize).into() } }
+    impl<Mod: Modulus> From<u64> for ModInt<Mod> { fn from(x: u64) -> Self { (x as usize).into() } }
+    impl<Mod: Modulus> From<i32> for ModInt<Mod> { fn from(x: i32) -> Self { (x as isize).into() } }
+    impl<Mod: Modulus> From<i64> for ModInt<Mod> { fn from(x: i64) -> Self { (x as isize).into() } }
+    impl<Mod: Modulus> From<usize> for ModInt<Mod> { fn from(x: usize) -> Self { Self::raw(x % Mod::m()) } }
+    impl<Mod: Modulus> From<isize> for ModInt<Mod> {
         fn from(mut x: isize) -> Self {
-            x %= MOD as isize;
-            if x < 0 { x += MOD as isize; }
-            ModInt(x as usize)
+            x %= Mod::m() as isize;
+            if x < 0 { x += Mod::m() as isize; }
+            ModInt::raw(x as usize)
         }
     }
-    impl std::fmt::Display for ModInt {
+    impl<Mod: Modulus> std::fmt::Display for ModInt<Mod> {
         fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> { write!(f, "{}", self.0) }
+    }
+
+    impl<Mod: Modulus> std::fmt::Debug for ModInt<Mod> {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> { write!(f, "mint({})", self.0) }
     }
 }
 
 #[allow(unused_imports)]
 use modint::*;
 
+#[allow(dead_code)]
+type Mint = ModInt<ConstModulus<998_244_353>>;
+// type Mint = ModInt<ConstModulus<1_000_000_7>>;
+
 #[cfg(test)]
 mod test {
-    use super::modint::*;
+    use super::Mint;
 
     #[test]
     fn test_modint() {
-        let a = ModInt(42);
-        let b = ModInt(43);
-        assert_eq!(a + b, ModInt(85), "Op +");
-        assert_eq!(a - b, ModInt(MOD - 1), "Op -");
-        assert_eq!(a * b, ModInt(1806), "Op *");
-        assert_eq!(a / b, ModInt(487_514_685), "Op /");
+        let a = Mint::raw(42);
+        let b = Mint::raw(43);
+        assert_eq!(a + b, Mint::raw(85), "Op +");
+        assert_eq!(a - b, Mint::raw(Mint::m() - 1), "Op -");
+        assert_eq!(a * b, Mint::raw(1806), "Op *");
+        assert_eq!(a / b, Mint::raw(487_514_685), "Op /");
     }
 
     #[test]
     fn test_with_primitive() {
-        let a = ModInt::from(42);
+        let a = Mint::from(42);
         let b = 43;
-        assert_eq!(a + b, ModInt(85), "Op +");
-        assert_eq!(a - b, ModInt(MOD - 1), "Op -");
-        assert_eq!(a * b, ModInt(1806), "Op *");
-        assert_eq!(a / b, ModInt(487_514_685), "Op /");
+        assert_eq!(a + b, Mint::raw(85), "Op +");
+        assert_eq!(a - b, Mint::raw(Mint::m() - 1), "Op -");
+        assert_eq!(a * b, Mint::raw(1806), "Op *");
+        assert_eq!(a / b, Mint::raw(487_514_685), "Op /");
     }
 
     #[test]
     fn test_assign_ops() {
-        let mut a = ModInt(42);
-        let b = ModInt(43);
+        let mut a = Mint::raw(42);
+        let b = Mint::raw(43);
         a += b;
-        assert_eq!(a, ModInt(85), "Op +=");
+        assert_eq!(a, Mint::raw(85), "Op +=");
         a -= b;
-        assert_eq!(a, ModInt(42), "Op -=");
+        assert_eq!(a, Mint::raw(42), "Op -=");
         a *= b;
-        assert_eq!(a, ModInt(1806), "Op *=");
+        assert_eq!(a, Mint::raw(1806), "Op *=");
         a /= b;
-        assert_eq!(a, ModInt(42), "Op /=");
+        assert_eq!(a, Mint::raw(42), "Op /=");
     }
 
     #[test]
     fn test_pow() {
-        let a = ModInt(42);
-        assert_eq!(a.pow(0), ModInt(1), "Pow 0");
-        assert_eq!(a.pow(1), ModInt(42), "Pow 1");
-        assert_eq!(a.pow(2), ModInt(1764), "Pow 2");
-        assert_eq!(a.pow(3), ModInt(74_088), "Pow 3");
+        let a = Mint::raw(42);
+        assert_eq!(a.pow(0), Mint::raw(1), "Pow 0");
+        assert_eq!(a.pow(1), Mint::raw(42), "Pow 1");
+        assert_eq!(a.pow(2), Mint::raw(1764), "Pow 2");
+        assert_eq!(a.pow(3), Mint::raw(74_088), "Pow 3");
     }
 }
--- rs/src/number_trait.rs
+++ rs/src/number_trait.rs
@@ -8,3 +8,29 @@ pub trait Number: PartialEq + Copy + Default + Add<Output = Self> + Sub<Output =
 }
 #[rustfmt::skip]
 impl<T> Number for T where T: PartialEq + Copy + Default + Add<Output = Self> + Sub<Output = Self> + Rem<Output = Self> + Mul<Output = Self> + Div<Output = Self> + From<bool> + Display { }
+
+// https://stackoverflow.com/a/61189128
+// limitation: can not accept `const` generics
+// see modint.rs for example usage
+#[macro_export]
+macro_rules! def_op {
+    (
+        $type:ident $(::< $( $lt:tt $( : $clt:tt $(+ $dlt:tt )* )? ),+ >)?
+        : $trait:ident :: $fn:ident | $a_trait:ident :: $a_fn:ident
+        => (&mut $self_param: ident, $rhs_param: ident) $impl:block
+    ) => {
+        impl<
+            $($( $lt$( : $clt$(+ $dlt)* )? ,)+ )?
+            Rhs: Into<$type$(< $($lt),+ >)?>
+        > std::ops::$trait<Rhs> for $type$(< $($lt),+ >)? {
+            type Output = Self;
+            fn $fn(mut self, rhs: Rhs) -> Self { self.$a_fn(rhs); self }
+        }
+        impl<
+            $($( $lt $( : $clt $(+ $dlt)* )? ),+ ,)?
+            Rhs: Into<$type$(< $($lt),+ >)?>
+        > std::ops::$a_trait<Rhs> for $type$(< $($lt),+ >)? {
+            fn $a_fn(&mut $self_param, $rhs_param: Rhs) $impl
+        }
+    };
+}
