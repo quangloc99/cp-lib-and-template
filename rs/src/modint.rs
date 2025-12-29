4e7bd06ca93811748a1d154201c5d3f0aa8a8972
Tran Quang Loc <quangloctran99@gmail.com>
2025-12-29 21:30:24 +0700

    Use u32 for modint instead of usize

--- rs/src/modint.rs
+++ rs/src/modint.rs
@@ -4,22 +4,22 @@ pub mod modint {
     use crate::def_op;
 
     pub trait Modulus: Clone + Copy + PartialEq + Eq + Default {
-        fn m() -> usize;
+        fn m() -> u32;
+        #[inline(always)] fn reduce(x: u64) -> u32 { (x % (Self::m() as u64)) as u32 }
     }
 
     #[derive(Clone, Copy, PartialEq, Eq, Default)]
-    pub struct ConstModulus<const M: usize>;
-    impl<const M: usize> Modulus for ConstModulus<M> {
-        fn m() -> usize { M }
-    }
+    pub struct ConstModulus<const M: u32>;
+    impl<const M: u32> ConstModulus<M> {}
+    impl<const M: u32> Modulus for ConstModulus<M> { fn m() -> u32{ M } }
 
     #[derive(Clone, Copy, Eq, PartialEq, Default)]
-    pub struct ModInt<Mod: Modulus>(pub usize, pub Mod);
+    pub struct ModInt<Mod: Modulus>(pub u32, pub Mod);
 
     #[allow(dead_code)]
     impl<Mod: Modulus> ModInt<Mod> {
-        pub fn m() -> usize { Mod::m() }
-        pub fn raw(x: usize) -> Self { Self(x, Mod::default()) }
+        pub fn m() -> u32{ Mod::m() }
+        pub fn raw(x: u32) -> Self { Self(x, Mod::default()) }
         pub fn pow(mut self, mut e: usize) -> Self {
             let mut result = Self::raw(1);
             while e > 0 {
@@ -29,25 +29,25 @@ pub mod modint {
             }
             result
         }
-        pub fn inv(self) -> Self { self.pow(Mod::m() - 2) }
+        pub fn inv(self) -> Self { self.pow((Mod::m() - 2) as usize) }
     }
 
     def_op!(ModInt::<Mod: Modulus>: Add::add | AddAssign::add_assign => (&mut self, rhs) { self.0 += rhs.into().0; if self.0 >= Mod::m() { self.0 -= Mod::m(); } });
     def_op!(ModInt::<Mod: Modulus>: Sub::sub | SubAssign::sub_assign => (&mut self, rhs) { self.0 = self.0.wrapping_sub(rhs.into().0); if self.0 >= Mod::m() { self.0 = self.0.wrapping_add(Mod::m()); } });
-    def_op!(ModInt::<Mod: Modulus>: Mul::mul | MulAssign::mul_assign => (&mut self, rhs) { self.0 = self.0 * rhs.into().0 % Mod::m(); });
+    def_op!(ModInt::<Mod: Modulus>: Mul::mul | MulAssign::mul_assign => (&mut self, rhs) { self.0 = Mod::reduce((self.0 as u64) * (rhs.into().0 as u64)) });
     def_op!(ModInt::<Mod: Modulus>: Div::div | DivAssign::div_assign => (&mut self, rhs) { *self *= rhs.into().inv(); });
 
     impl<Mod: Modulus> From<String> for ModInt<Mod> { fn from(s: String) -> Self { s.parse::<usize>().unwrap().into() } }
-    impl<Mod: Modulus> From<u32> for ModInt<Mod> { fn from(x: u32) -> Self { (x as usize).into() } }
-    impl<Mod: Modulus> From<u64> for ModInt<Mod> { fn from(x: u64) -> Self { (x as usize).into() } }
-    impl<Mod: Modulus> From<i32> for ModInt<Mod> { fn from(x: i32) -> Self { (x as isize).into() } }
-    impl<Mod: Modulus> From<i64> for ModInt<Mod> { fn from(x: i64) -> Self { (x as isize).into() } }
-    impl<Mod: Modulus> From<usize> for ModInt<Mod> { fn from(x: usize) -> Self { Self::raw(x % Mod::m()) } }
-    impl<Mod: Modulus> From<isize> for ModInt<Mod> {
-        fn from(mut x: isize) -> Self {
-            x %= Mod::m() as isize;
-            if x < 0 { x += Mod::m() as isize; }
-            ModInt::raw(x as usize)
+    impl<Mod: Modulus> From<u32> for ModInt<Mod> { fn from(x: u32) -> Self { Self::raw(x % Mod::m()) } }
+    impl<Mod: Modulus> From<u64> for ModInt<Mod> { fn from(x: u64) -> Self { Self::raw(Mod::reduce(x)) } }
+    impl<Mod: Modulus> From<usize> for ModInt<Mod> { fn from(x: usize) -> Self { (x as u64).into() } }
+    impl<Mod: Modulus> From<i32> for ModInt<Mod> { fn from(x: i32) -> Self { (x as i64).into() } }
+    impl<Mod: Modulus> From<isize> for ModInt<Mod> { fn from(x: isize) -> Self { (x as i64).into() } }
+    impl<Mod: Modulus> From<i64> for ModInt<Mod> {
+        fn from(mut x: i64) -> Self {
+            x %= Mod::m() as i64;
+            if x < 0 { x += Mod::m() as i64; }
+            ModInt::raw(x as u32)
         }
     }
     impl<Mod: Modulus> std::fmt::Display for ModInt<Mod> {
