d10a5249605a20dd129e5bd718ef2e4dd115f098
Tran Quang Loc <quangloctran99@gmail.com>
2025-12-30 18:58:50 +0700

    Optimization and restructure fft

--- rs/src/fft.rs
+++ rs/src/fft.rs
@@ -1,100 +1,117 @@
 use crate::modint::modint::*;
 use crate::number_trait::*;
 
-trait FFTOmegaNum: Number {
-    fn omega(max_lv: usize) -> impl FFTOmega<Self>;
-}
-
-trait FFTOmega<Num: FFTOmegaNum> {
-    fn set_lv(&mut self, lv: usize) -> Num;
-    fn next(&mut self, w: &mut Num);
-}
-
-fn fft<Num: FFTOmegaNum>(a: &mut [Num]) {
-    let n = a.len();
-    let l = n.trailing_zeros() as usize;
-    assert!(n == 1 << l);
-
-    let a = a.as_mut_ptr();
-    {
-        let mut rev_i = 0;
-        for i in 0..n {
-            if rev_i < i {
-                unsafe { (*a.add(i), *a.add(rev_i)) = (*a.add(rev_i), *a.add(i)) }
-            }
-            let mut bit = 1 << l;
-            loop {
-                bit >>= 1;
-                rev_i ^= bit;
-                if rev_i & bit == bit {
-                    break;
-                }
-            }
-        }
-    }
-
-    let mut omega = Num::omega(l);
-
-    for lv in 1..=l {
-        let s = 1 << lv;
-        for start in (0..n).step_by(s) {
-            unsafe {
-                let mut w = omega.set_lv(lv);
-                let chunk = a.add(start);
-                for (i, j) in (s / 2..s).enumerate() {
-                    let (au, av) = (chunk.add(i), chunk.add(j));
-                    let (u, v) = (*au, *av * w);
-                    (*au, *av) = (u + v, u - v);
-                    omega.next(&mut w);
-                }
-            }
-        }
-    }
-}
-
-fn convolution<Num: FFTOmegaNum>(a: Vec<Num>, mut b: Vec<Num>) -> Vec<Num> {
-    let s = a.len() + b.len() - 1;
-    let n = s.next_power_of_two();
-    b.resize(n, Num::zero());
-    fft(&mut b);
-    partial_conv(a, &b)
-}
-
-/// Here b is already convoluted. Can help save one fft and also memory
-fn partial_conv<Num: FFTOmegaNum>(mut a: Vec<Num>, conv_b: &[Num]) -> Vec<Num> {
-    let n = conv_b.len();
-    assert!(n.is_power_of_two());
-    assert!(a.len() <= n);
-    a.resize(n, Num::zero());
-    fft(&mut a);
-    let inv = Num::one() / Num::from(n);
-    for i in 0..n {
-        a[i] = a[i] * conv_b[i] * inv;
-    }
-    a[1..].reverse();
-    fft(&mut a);
-
-    while a.last() == Some(&Num::zero()) {
-        a.pop();
-    }
-    a
+pub trait FFTOmegaNum: Number {
+    type Omega: FFTOmega<Self>;
+    fn omega() -> Self::Omega;
 }
 
 #[rustfmt::skip]
+pub trait FFTOmega<Num: FFTOmegaNum> {
+    type OmegaLv<'a>: Iterator<Item = Num> where Self: 'a;
+    fn get_lv<'a>(&'a self, lv: usize) -> Self::OmegaLv<'a>;
+}
+
+#[rustfmt::skip]
+struct FFT<Num: FFTOmegaNum> {
+    omega: Num::Omega,
+}
+
+#[rustfmt::skip]
+impl<Num: FFTOmegaNum> FFT<Num> {
+    pub fn new() -> Self { Self { omega: Num::omega() } }
+
+    pub fn fft(&self, a: &mut [Num]) {
+        let n = a.len();
+        let l = n.trailing_zeros() as usize;
+        assert!(n == 1 << l);
+
+        for i in 1..n {
+            let rev_i = i.reverse_bits() >> (usize::BITS as usize - l);
+            if rev_i < i { a.swap(i, rev_i); }
+        }
+
+        for lv in 1..=l {
+            for chunks in a.chunks_exact_mut(1 << lv) {
+                let (ci, cj) = chunks.split_at_mut(1 << lv >> 1);
+                for ((au, av), w) in ci.iter_mut().zip(cj.iter_mut()).zip(self.omega.get_lv(lv)) {
+                    let (u, vw) = (*au, *av * w);
+                    (*au, *av) = (u + vw, u - vw);
+                }
+            }
+        }
+    }
+
+    pub fn convolution(&self, a: Vec<Num>, b: Vec<Num>) -> Vec<Num> {
+        if a.len().min(b.len()) <= 150 { self.brute_force_convolution(a, b) } else { self.fft_convolution(a, b) }
+    }
+
+    pub fn fft_convolution(&self, a: Vec<Num>, mut b: Vec<Num>) -> Vec<Num> {
+        let s = a.len() + b.len() - 1;
+        let n = s.next_power_of_two();
+        b.resize(n, Num::zero());
+        self.fft(&mut b);
+        self.partial_conv(a, &b)
+    }
+
+    /// Here b is already convoluted. Can help save one fft and also memory
+    pub fn partial_conv(&self, mut a: Vec<Num>, conv_b: &[Num]) -> Vec<Num> {
+        let n = conv_b.len();
+        assert!(n.is_power_of_two() && a.len() <= n);
+        a.resize(n, Num::zero());
+        if n == 1 {
+            a[0] = a[0] * conv_b[0];
+        } else {
+            self.fft(&mut a);
+            let inv = Num::one() / Num::from(n);
+            (a[0], a[n / 2]) = (a[0] * conv_b[0] * inv, a[n / 2] * conv_b[n / 2] * inv);
+            for i in 1..n / 2 {
+                let j = n - i;
+                (a[j], a[i]) = (a[i] * conv_b[i] * inv, a[j] * conv_b[j] * inv);
+            }
+            self.fft(&mut a);
+        }
+
+        while a.last() == Some(&Num::zero()) { a.pop(); }
+        a
+    }
+
+    pub fn brute_force_convolution(&self, mut a: Vec<Num>, b: Vec<Num>) -> Vec<Num> {
+        if a.len() < b.len() { return self.brute_force_convolution(b, a); }
+        if a.is_empty() || b.is_empty() { return a; }
+        let (max_ai, s) = (a.len() - 1, a.len() + b.len() - 1);
+        a.resize(s, Num::zero());
+        for i in (0..s).rev() {
+            let start = i.checked_sub(max_ai).unwrap_or(0);
+            a[i] = (start..b.len().min(i + 1)).fold(Num::zero(), |acc, j| acc + a[i - j] * b[j]);
+        }
+        while a.last() == Some(&Num::zero()) { a.pop(); }
+        a
+    }
+}
+
+pub struct MintOmega<M: 'static + Modulus> {
+    precal: Vec<ModInt<M>>,
+}
+#[rustfmt::skip]
+impl<M: 'static + Modulus> FFTOmega<ModInt<M>> for MintOmega<M> where ModInt<M>: FFTOmegaNum {
+    type OmegaLv<'a> = std::iter::Copied<std::slice::Iter<'a, ModInt<M>>>;
+    #[inline(always)] fn get_lv<'a>(&'a self, lv: usize) -> Self::OmegaLv<'a> { self.precal[1 << lv..2 << lv].iter().copied() }
+}
+
 macro_rules! ModIntFFTOmegaNumImpl {
     ($(($MOD:expr, $ROOT:expr))*) => {
         $(impl FFTOmegaNum for ModInt<ConstModulus<$MOD>> {
-            fn omega(_lv: usize) -> impl FFTOmega<Self> {
+            type Omega = MintOmega<ConstModulus<$MOD>>;
+            fn omega() -> Self::Omega {
                 type Mint = ModInt<ConstModulus<$MOD>>;
-                struct Omega { precal: &'static [Mint], omega: Mint, }
-                impl FFTOmega<Mint> for Omega {
-                    fn set_lv(&mut self, lv: usize) -> Mint { self.omega = self.precal[lv]; Mint::one() }
-                    fn next(&mut self, w: &mut Mint) { *w *= self.omega; }
+                const MAX_LV: u32 = ($MOD - 1usize).trailing_zeros();
+                let mut precal = vec![Mint::one(); 1 << MAX_LV];
+                for lv in 0..MAX_LV {
+                    let omega = Mint::from($ROOT).pow($MOD as usize >> lv);
+                    for i in ((1 << lv) + 1)..2 << lv { precal[i] = precal[i - 1] * omega; }
                 }
-                use std::sync::OnceLock;
-                static PRECAL: OnceLock<Vec<Mint>> = OnceLock::<Vec<Mint>>::new();
-                let precal = PRECAL.get_or_init(|| (0..32).map(|i| Mint::from($ROOT).pow($MOD >> i)).collect());
-                Omega { precal, omega: Mint::default() }
+                MintOmega { precal }
             }
         })*
     }
@@ -126,19 +143,26 @@ pub mod test_fft {
     }
 
     fn _test(num_cases: usize, n_max: usize, val_max: usize) {
+        let fft = FFT::<Mint>::new();
         for testcase in 0..num_cases {
             let mut rng = create_prng(testcase as u64);
             let n = (rng() as usize % n_max) + 1;
             let m = (rng() as usize % n_max) + 1;
             let a: Vec<Mint> = (0..n).map(|_| Mint::from(rng() as usize % val_max)).collect();
             let b: Vec<Mint> = (0..m).map(|_| Mint::from(rng() as usize % val_max)).collect();
-            let expected = brute_force_convolution(&a, &b);
-            let actual = convolution(a.clone(), b.clone());
+            let expected = checker_convolution(&a, &b);
+            let actual = fft.convolution(a.clone(), b.clone());
+            let actual_fft_only = fft.fft_convolution(a.clone(), b.clone());
             assert_eq!(expected, actual, "testing\na={:?}\nb={:?}\ntestcase={}", a, b, testcase);
+            assert_eq!(
+                expected, actual_fft_only,
+                "testing\na={:?}\nb={:?}\ntestcase={}",
+                a, b, testcase
+            );
         }
     }
 
-    fn brute_force_convolution(a: &[Mint], b: &[Mint]) -> Vec<Mint> {
+    fn checker_convolution(a: &[Mint], b: &[Mint]) -> Vec<Mint> {
         let s = a.len() + b.len() - 1;
         let mut c = vec![Mint::zero(); s];
         for i in 0..a.len() {
